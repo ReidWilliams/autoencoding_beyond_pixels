99fda77531e61ed1a887c286bf7ba59a96d0278d
Reid Williams <rwilliams@ideo.com>
2017-10-06 23:08:51 -0700

    skipping visual attribute steps

--- celeba_aegan.py
+++ celeba_aegan.py
@@ -43,48 +43,48 @@ def run():
     with open(model_path, 'wb') as f:
         pickle.dump(model, f)
 
-    print('Extracting visual attribute vectors')
-    model.phase = 'test'
-    train_feed, test_feed = dataset.celeba.feeds(
-        img_size, batch_size=batch_size, epoch_size=epoch_size,
-        with_attributes=True, split='test',
-    )
+    # print('Extracting visual attribute vectors')
+    # model.phase = 'test'
+    # train_feed, test_feed = dataset.celeba.feeds(
+    #     img_size, batch_size=batch_size, epoch_size=epoch_size,
+    #     with_attributes=True, split='test',
+    # )
 
-    n_attr_imgs = 10000
-    x = img_transform(train_feed.x[:n_attr_imgs], to_bc01=False)
-    y = train_feed.y[:n_attr_imgs]
-    z = model.encode(x)
+    # n_attr_imgs = 10000
+    # x = img_transform(train_feed.x[:n_attr_imgs], to_bc01=False)
+    # y = train_feed.y[:n_attr_imgs]
+    # z = model.encode(x)
 
-    all_attributes = list(dp.dataset.CelebA().attribute_names)
-    selected_attributes = [
-        'Bald', 'Bangs', 'Black_Hair', 'Blond_Hair', 'Bushy_Eyebrows',
-        'Eyeglasses', 'Gray_Hair', 'Heavy_Makeup', 'High_Cheekbones', 'Male',
-        'Mustache', 'Pale_Skin', 'Rosy_Cheeks', 'Smiling', 'Straight_Hair',
-        'Wavy_Hair', 'Wearing_Lipstick', 'Young',
-    ]
-    attr_idxs = [all_attributes.index(attr) for attr in selected_attributes]
-    attr_vecs = []
-    for attr_idx in attr_idxs:
-        on_mask = y[:, attr_idx] == 1.0
-        off_mask = np.logical_not(on_mask)
-        vec = (np.mean(z[on_mask, :], axis=0, dtype=float) -
-               np.mean(z[off_mask, :], axis=0, dtype=float))
-        attr_vecs.append(vec)
+    # all_attributes = list(dp.dataset.CelebA().attribute_names)
+    # selected_attributes = [
+    #     'Bald', 'Bangs', 'Black_Hair', 'Blond_Hair', 'Bushy_Eyebrows',
+    #     'Eyeglasses', 'Gray_Hair', 'Heavy_Makeup', 'High_Cheekbones', 'Male',
+    #     'Mustache', 'Pale_Skin', 'Rosy_Cheeks', 'Smiling', 'Straight_Hair',
+    #     'Wavy_Hair', 'Wearing_Lipstick', 'Young',
+    # ]
+    # attr_idxs = [all_attributes.index(attr) for attr in selected_attributes]
+    # attr_vecs = []
+    # for attr_idx in attr_idxs:
+    #     on_mask = y[:, attr_idx] == 1.0
+    #     off_mask = np.logical_not(on_mask)
+    #     vec = (np.mean(z[on_mask, :], axis=0, dtype=float) -
+    #            np.mean(z[off_mask, :], axis=0, dtype=float))
+    #     attr_vecs.append(vec)
 
-    print('Outputting visual attribute vectors')
-    original_x = test_feed.batches().next()[0]
-    original_z = model.encode(original_x)
-    attributes_dir = os.path.join(output_dir, 'attributes')
-    if not os.path.exists(attributes_dir):
-        os.mkdir(attributes_dir)
-    for attr_idx, attr_vec in zip(attr_idxs, attr_vecs):
-        attr_name = all_attributes[attr_idx].lower()
-        attrs_z = original_z + attr_vec
-        attrs_x = model.decode(attrs_z.astype(dp.float_))
-        attrs_x = img_inverse_transform(attrs_x)
-        for i, attr_x in enumerate(attrs_x):
-            path = os.path.join(attributes_dir, '%.3d_%s.png' % (i, attr_name))
-            sp.misc.imsave(path, attr_x)
+    # print('Outputting visual attribute vectors')
+    # original_x = test_feed.batches().next()[0]
+    # original_z = model.encode(original_x)
+    # attributes_dir = os.path.join(output_dir, 'attributes')
+    # if not os.path.exists(attributes_dir):
+    #     os.mkdir(attributes_dir)
+    # for attr_idx, attr_vec in zip(attr_idxs, attr_vecs):
+    #     attr_name = all_attributes[attr_idx].lower()
+    #     attrs_z = original_z + attr_vec
+    #     attrs_x = model.decode(attrs_z.astype(dp.float_))
+    #     attrs_x = img_inverse_transform(attrs_x)
+    #     for i, attr_x in enumerate(attrs_x):
+    #         path = os.path.join(attributes_dir, '%.3d_%s.png' % (i, attr_name))
+    #         sp.misc.imsave(path, attr_x)
 
 
 if __name__ == '__main__':
